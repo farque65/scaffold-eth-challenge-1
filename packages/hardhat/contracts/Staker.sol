b6272e2d76a2212a0d1aebcd2c7e1c1dad44584c
farque65 <fahim.farque@gmail.com>
2021-09-24 09:59:42 -0400

    challenge 1 part 1

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,30 +1,29 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract Staker {
-
+  // External contract that will old stacked funds
   ExampleExternalContract public exampleExternalContract;
+
+  // Balances of the user's stacked funds
+  mapping(address => uint256) public balances;
+
+  // Staking threshold
+  uint public constant threshold = 1 ether;
+
+  // Contract's Events
+  event Stake(address indexed sender, uint256 amount);
 
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+    // update the user's balance
+    balances[msg.sender] += msg.value;
 
-
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
-
-
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
+    //emit the event to notify the blockchain that we have correctly Staked some fund for the user
+    emit Stake(msg.sender, msg.value);
+  }
 }
